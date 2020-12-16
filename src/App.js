f944b095601d02ad55c59af37b0589a83e747727
Jawaria Alvi <jawariaalvi32@gmail.com>
2020-12-17 01:19:16 +0500

    Styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Header from './components/Header';
 import Api from './components/global/Api';
 import { CountryApi } from './components/Countries/Api';
+import Grid from '@material-ui/core/Grid';
+import Logo from './images/covid.gif'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Grid container>
+        <Grid item md={6} sm={12}>
+        <img src={Logo} alt="logo" width="100%"/>
+        </Grid>
+        <Grid item md={4} sm={12}>
+        <Header/>
       <Api/>
       <CountryApi/>
+        </Grid>
+      <Grid item md={2} sm={0}/>
+        </Grid>
     </div>
   );
 }
